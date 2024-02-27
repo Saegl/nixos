10e3dfbff80d69bb10f2bd0b6779187881c63b3d
Saegl <saegl@protonmail.com>
2024-02-28 02:08:17 +0600

    Extract nix-ld settings to nixld.nix

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./nixld.nix
     ];
 
   boot.loader.systemd-boot.enable = true;
@@ -71,60 +72,6 @@
   nixpkgs.config.cudaSupport = true;
   nixpkgs.config.permittedInsecurePackages = [
     "electron-25.9.0"  # TODO Unfortunately needed for obsidian, remove when fixed
-  ];
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    alsa-lib
-    at-spi2-atk
-    at-spi2-core
-    atk
-    cairo
-    cups
-    curl
-    dbus
-    expat
-    fontconfig
-    freetype
-    fuse3
-    gdk-pixbuf
-    glib
-    gtk3
-    icu
-    libGL
-    libappindicator-gtk3
-    libdrm
-    libglvnd
-    libnotify
-    libpulseaudio
-    libunwind
-    libusb1
-    libuuid
-    libxkbcommon
-    libxml2
-    mesa
-    nspr
-    nss
-    openssl
-    pango
-    pipewire
-    stdenv.cc.cc
-    systemd
-    vulkan-loader
-    xorg.libX11
-    xorg.libXScrnSaver
-    xorg.libXcomposite
-    xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXtst
-    xorg.libxcb
-    xorg.libxkbfile
-    xorg.libxshmfence
-    zlib
   ];
 
   services.asusd.enable = true;
