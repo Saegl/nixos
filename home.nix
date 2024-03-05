18bbef92804cdea8d78909cd7e57ec11b92f70de
Saegl <saegl@protonmail.com>
2024-03-05 07:04:13 +0500

    Add git shortcuts

--- home.nix
+++ home.nix
@@ -162,8 +162,14 @@
       unsetld = "set -u LD_LIBRARY_PATH";
       rebuild = "sudo nixos-rebuild switch";
       wipe-history = "sudo nix profile wipe-history --older-than 7d --profile /nix/var/nix/profiles/system";
-      gc = "sudo nix store gc --debug";
+      storegc = "sudo nix store gc --debug";
       ":q" = "exit";
+
+      gs = "git status";
+      gd = "git diff";
+      ga = "git add";
+      gc = "git commit";
+      ghb = "gh browse";
     };
   };
 
