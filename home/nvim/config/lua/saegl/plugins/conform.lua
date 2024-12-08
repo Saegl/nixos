0924e849694229b52291fcd66a59628bcbecd6d5
Saegl <saegl@protonmail.com>
2024-12-08 16:41:06 +0500

    Add nvim autoformat toggle

--- home/nvim/config/lua/saegl/plugins/conform.lua
+++ home/nvim/config/lua/saegl/plugins/conform.lua
@@ -15,6 +15,9 @@ return {
         opts = {
             notify_on_error = false,
             format_on_save = function(bufnr)
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
                 -- Disable "format_on_save lsp_fallback" for languages that don't
                 -- have a well standardized coding style. You can add additional
                 -- languages here or re-enable it for the disabled ones.
@@ -33,5 +36,26 @@ return {
                 -- javascript = { { "prettierd", "prettier" } },
             },
         },
+        config = function(_, opts)
+            local conform = require 'conform'
+            conform.setup(opts)
+
+            vim.api.nvim_create_user_command("FormatDisable", function(args)
+                vim.g.disable_autoformat = true
+            end, {
+                desc = "Disable autoformat-on-save",
+            })
+            vim.api.nvim_create_user_command("FormatFileDisable", function(args)
+                vim.b.disable_autoformat = true
+            end, {
+                desc = "Disable autoformat-on-save for file",
+            })
+            vim.api.nvim_create_user_command("FormatEnable", function()
+                vim.b.disable_autoformat = false
+                vim.g.disable_autoformat = false
+            end, {
+                desc = "Re-enable autoformat-on-save",
+            })
+        end
     },
 }
