50aafb276445366578a744a4afbacb99c66e609c
Saegl <saegl@protonmail.com>
2025-01-05 17:10:42 +0500

    Add nixos files picker

--- home/nvim/config/lua/saegl/plugins/telescope.lua
+++ home/nvim/config/lua/saegl/plugins/telescope.lua
@@ -59,6 +59,10 @@ return {
             vim.keymap.set('n', '<leader>sn', function()
                 builtin.find_files { cwd = vim.fn.stdpath 'config' }
             end, { desc = '[S]earch [N]eovim files' })
+
+            vim.keymap.set('n', '<leader>sx', function()
+                builtin.find_files { cwd = '/home/saegl/projects/nix/nixos' }
+            end, { desc = '[S]earch Ni[x] files' })
         end,
     },
 }
