c26c3dab2c30e27a7ce0f1e05b9a743af1c8e4b7
Saegl <saegl@protonmail.com>
2025-01-13 18:42:59 +0500

    Get more local ipv4 addresses

--- os/networking.nix
+++ os/networking.nix
@@ -4,6 +4,13 @@
   networking.hostName = "frostmourne";
   networking.firewall.enable = false;
 
+  networking.interfaces."lo".ipv4.addresses = [
+    {
+      address = "127.0.0.2";
+      prefixLength = 32;
+    }
+  ];
+
   networking.stevenblack = {
     enable = false;
     block = ["fakenews" "gambling" "porn" "social"];
