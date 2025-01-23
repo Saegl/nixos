128f908d18f581c9c4bde82a1ebf86cab0eafc5d
Saegl <saegl@protonmail.com>
2025-01-23 15:30:07 +0500

    Use waybar again, with minimal style

--- home/waybar/waybar.nix
+++ home/waybar/waybar.nix
@@ -6,4 +6,5 @@
   programs.waybar.enable = true;
   home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
   home.file.".config/waybar/style.css".source = ./style.css;
+  home.file.".config/waybar/style_minimal.css".source = ./style_minimal.css;
 }
