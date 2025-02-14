6cc0dacca083acee4377ce2f071c86465d42a195
Saegl <saegl@protonmail.com>
2025-02-14 19:41:57 +0500

    Remove unused nvim plugins

--- home/nvim/config/lua/saegl/plugins/motions.lua
+++ home/nvim/config/lua/saegl/plugins/motions.lua
@@ -1,12 +1,5 @@
 return {
     {
-        'ggandor/leap.nvim',
-        enabled = false,
-        config = function()
-            require('leap').create_default_mappings()
-        end,
-    },
-    {
         "folke/flash.nvim",
         event = "VeryLazy",
         enabled = true,
