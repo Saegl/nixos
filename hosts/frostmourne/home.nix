163a453354b0f5cb71de2e95e3bcda88cd20ac10
Saegl <saegl@protonmail.com>
2025-02-23 21:09:50 +0500

    Remove homebundle.nix

--- hosts/frostmourne/home.nix
+++ hosts/frostmourne/home.nix
@@ -1,6 +1,12 @@
 {...}: {
   imports = [
-    ./../../home/homebundle.nix
+    ./../../home/dev.nix
+    ./../../home/xdg.nix
+    ./../../home/gtk.nix
+    ./../../home/fish.nix
+    ./../../home/apps.nix
+    ./../../home/python.nix
+    ./../../home/dotfiles.nix
   ];
   home.username = "saegl";
   home.homeDirectory = "/home/saegl";
