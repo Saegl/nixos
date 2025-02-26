50a7b0086301c53275892b9cc99732ee8c7e1581
Saegl <saegl@protonmail.com>
2025-02-27 00:03:16 +0500

    Easy way to fix home-manager dotfiles

--- hosts/frostmourne/configuration.nix
+++ hosts/frostmourne/configuration.nix
@@ -12,7 +12,7 @@
     {
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
-      home-manager.backupFileExtension = "backup";
+      home-manager.backupFileExtension = "backup2";
       home-manager.users.saegl = import ./home.nix;
       home-manager.extraSpecialArgs = {inherit inputs;};
     }
