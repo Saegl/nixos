40d0efa9239f1d43a0637f4cbd2d953ea6cc312b
Saegl <saegl@protonmail.com>
2025-02-27 00:04:03 +0500

    Expose openssl vars

--- hosts/frostmourne/configuration.nix
+++ hosts/frostmourne/configuration.nix
@@ -66,6 +66,9 @@
     QT_SCREEN_SCALE_FACTORS = "2";
     CUDA_PATH = "${pkgs.cudaPackages.cudatoolkit}";
     CUDNN_PATH = "${pkgs.cudaPackages.cudnn.lib}";
+    OPENSSL_DIR = "${pkgs.openssl.dev}";
+    OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+    OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
   };
   environment.systemPackages = with pkgs; [
     # basic
