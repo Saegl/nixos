c715fb8d71e184bdb0b7d83fdadaec20451252b5
Saegl <saegl@protonmail.com>
2025-03-05 14:21:21 +0500

    Show list of opened terminals in lualine

--- dotfiles/nvim/lua/saegl/plugins/lualine.lua
+++ dotfiles/nvim/lua/saegl/plugins/lualine.lua
@@ -18,6 +18,15 @@ return {
                 },
             }
 
+            local function opened_terminals()
+                local terms = require("toggleterm.terminal").get_all()
+                local term_names = {}
+                for _, term in ipairs(terms) do
+                    table.insert(term_names, "T" .. term.id)
+                end
+                return table.concat(term_names, " | ")
+            end
+
             require('lualine').setup {
                 options = {
                     component_separators = '|',
@@ -28,7 +37,7 @@ return {
                 sections = {
                     lualine_a = { 'mode' },
                     lualine_b = { 'branch', 'diff', 'diagnostics' },
-                    lualine_c = { filename },
+                    lualine_c = { opened_terminals, filename },
                     lualine_x = { 'encoding', fileformat, 'filetype' },
                     lualine_y = { 'progress' },
                     lualine_z = { 'location' }
