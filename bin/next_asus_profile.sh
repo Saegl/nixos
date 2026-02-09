f5e0ba80b25a94f9d50c6a7221550df6e628f167
Saegl <saegl@protonmail.com>
2026-02-09 18:04:35 +0500

    Fix next asus profile

--- bin/next_asus_profile.sh
+++ bin/next_asus_profile.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Switch to the next profile
-asusctl profile -n
+asusctl profile next
 
 # Get the current profile
-CURRENT_PROFILE=$(asusctl profile -p | grep -v '\[INFO')
+CURRENT_PROFILE=$(asusctl profile get)
 
 # Send a notification
 notify-send "ASUS Profile Changed" "$CURRENT_PROFILE"
