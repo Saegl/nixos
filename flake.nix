f5da79a9b399cab8b2a363dd66f97c0b98cb43bd
Saegl <saegl@protonmail.com>
2026-04-05 01:44:30 +0500

    Remove nerzhul

--- flake.nix
+++ flake.nix
@@ -4,20 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {...} @ inputs: {
     nixosConfigurations."frostmourne" = inputs.nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [./frostmourne.nix];
-    };
-    nixOnDroidConfigurations."nerzhul" = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-      pkgs = import inputs.nixpkgs {system = "aarch64-linux";};
-      modules = [./nerzhul.nix];
     };
   };
 }
