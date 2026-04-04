0951e3de69ef3cc15edec4504afbad684ec6ee2e
Saegl <saegl@protonmail.com>
2026-04-05 01:46:15 +0500

    Remove unison configs

--- dotfiles/fish/init.fish
+++ dotfiles/fish/init.fish
@@ -51,7 +51,6 @@ if status is-interactive;
     # dotlink ~/.config/foot/foot.ini "$DOTFILES/foot/foot.ini"
     dotlink ~/.config/foot "$DOTFILES/foot"
     dotlink ~/.config/kitty/kitty.conf "$DOTFILES/kitty/kitty.conf"
-    dotlink ~/.unison/shared.prf "$DOTFILES/unison/shared.prf"
     dotlink ~/.config/starship.toml "$DOTFILES/starship/starship.toml"
     dotlink ~/.config/yazi/yazi.toml "$DOTFILES/yazi/yazi.toml"
     dotlink ~/.config/river/init "$DOTFILES/river/init"
